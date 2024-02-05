da72b0899a18b87e118b92bc1bdd24ae2dcba350
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-02-05 16:01:09 -0300

    Input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,8 +4,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
   StartCountdownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -14,11 +16,14 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
-
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
           <span>minutos.</span>
         </FormContainer>
 

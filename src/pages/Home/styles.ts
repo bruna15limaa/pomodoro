3ce1930944c817eda7932ddc04d0c779e2a8c9af
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-02-10 11:45:37 -0300

    ok

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -43,13 +43,14 @@ const BaseInput = styled.input`
   &::placeholder {
     color: ${(props) => props.theme['gray-500']};
   }
-
 `
-
-
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `
 
 export const MinutesAmountInput = styled(BaseInput)`

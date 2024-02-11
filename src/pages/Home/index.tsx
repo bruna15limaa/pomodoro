af2d769bd84375a211b89d2bce8bbc1ceb28dba4
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-02-11 12:23:26 -0300

    useState

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,10 +9,17 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
 
 
 
 export function Home() {
+  const [task, setTask] = useState('');
+
+  function resetForm() {
+    setTask('')
+  }
+
   return (
     <HomeContainer>
       <form>
@@ -22,6 +29,8 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
           />
 
           <datalist id="task-suggestions">
@@ -50,7 +59,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={!task} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>

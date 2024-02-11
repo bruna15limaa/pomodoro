4d6e5f85ff467613f5e7c03f393fd1262b9de70c
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-02-11 13:28:32 -0300

    zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -11,9 +13,19 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  MinutesAmount: zod
+  .number()
+  .min(5,'O valor precisa ser no mínino de 05 minutos.' )
+  .max(60, 'O valor precisa ser no máximo de 60 minutos.'),
+})
+
 export function Home() {  
 
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)

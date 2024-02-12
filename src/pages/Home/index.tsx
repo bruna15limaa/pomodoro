40b6b411264a04799ff1f994d9f1639ce9c68b5a
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-02-12 14:13:03 -0300

    button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Play } from 'phosphor-react'
+import { Hand, HandPalm, Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
@@ -12,6 +12,7 @@ import {
   MinutesAmountInput,
   Separator,
   StartCountdownButton,
+  StopCountdownButton,
   TaskInput,
 } from './styles'
 
@@ -128,10 +129,17 @@ export function Home() {
           <span>{seconds[1]}</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+        { activeCycle ? (
+          <StopCountdownButton type="button">
+            <HandPalm size={24} />
+            Interrromper
+          </StopCountdownButton>
+        ):(
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
+        )}
       </form>
     </HomeContainer>
   )
